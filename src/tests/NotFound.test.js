3e31d91e68d438ac3739a83d10e739ec1dbce196
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-10-20 15:47:12 -0300

    Requisito 4 concluído

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('1. Testa se a página contém um heading h2 com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+  });
+
+  test('2. Testa se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByRole('img');
+    expect(image).toBeInTheDocument();
+    expect(image.tagName).toBe('IMG');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
